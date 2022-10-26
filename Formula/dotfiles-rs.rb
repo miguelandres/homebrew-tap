a18a39e59f1093672eb5e584be3c9d7bf8730a4f
Miguel Barreto <miguelandres@users.noreply.github.com>
2022-10-25 21:09:44 -0400

    update dotfiles

--- Formula/dotfiles-rs.rb
+++ Formula/dotfiles-rs.rb
@@ -1,8 +1,8 @@
 class DotfilesRs < Formula
   desc "Dotfiles management framework"
   homepage "https://github.com/miguelandres/dotfiles"
-  url "https://github.com/miguelandres/dotfiles-rs/releases/download/nightly-2022-10-19/dotfiles-nightly-2022_10_19-macos-universal.tar.gz"
-  sha256 "672743d95447a039373f95b1f78dd868828d12018093faa110842435ff03d079"
+  url "https://github.com/miguelandres/dotfiles-rs/releases/download/nightly-2022-10-25/dotfiles-nightly-2022_10_25-macos-universal.tar.gz"
+  sha256 "f9ef500972ae1918387909340ff8fa316f7ac7ec3eb56f04366d014ba2a936db"
   license "MIT"
 
   def install
