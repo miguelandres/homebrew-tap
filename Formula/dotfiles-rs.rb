8ee57fcf55b06c2a6c59f38d4f242df8a9e26de0
Miguel Barreto <miguelandres@users.noreply.github.com>
2022-10-27 18:04:52 -0400

    make it build dotfiles

--- Formula/dotfiles-rs.rb
+++ Formula/dotfiles-rs.rb
@@ -1,11 +1,14 @@
 class DotfilesRs < Formula
   desc "Dotfiles management framework"
   homepage "https://github.com/miguelandres/dotfiles"
-  url "https://github.com/miguelandres/dotfiles-rs/releases/download/nightly-2022-10-27/dotfiles-nightly-2022_10_27-macos-universal.tar.gz"
-  sha256 "76ecf96f93ab8c3d527365b6face205d3fa77650678d1dd75286d4eb1e67b0b1"
+  url "https://api.github.com/repos/miguelandres/dotfiles-rs/tarball/nightly-2022-10-27"
+  sha256 "7029e0f7fcdbc66ca754081aadcc740f4680ce04023413d58f5b702ea86a7f7a"
   license "MIT"
 
+  depends_on "rustup-init"
+
   def install
+    system "rustup-init", "" 
     bin.install "dotfiles"
   end
 
--- download.sh
+++ download.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-URL=$(curl -s https://api.github.com/repos/miguelandres/dotfiles-rs/releases/latest \
-| grep "browser_download_url.*macos-universal" \
+os=${1=macos-universal}
+repo=${2="miguelandres/dotfiles"}
+
+URL=$(curl -s https://api.github.com/repos/$repo/releases/latest \
+| grep "browser_download_url.*$os" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | cut -c 2-)
 
-echo "$URL"
-curl $URL -L -s -o df.tar.gz
+echo "downloading $URL"
+curl "$URL" -L -o df.tar.gz
 
 shasum -a 256 df.tar.gz
-
 rm df.tar.gz
+
