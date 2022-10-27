81ba4960b60a7e114c90a7e09081348e386ca61e
Miguel Barreto <miguelandres@users.noreply.github.com>
2022-10-27 18:19:43 -0400

    fix style

--- Formula/dotfiles-rs.rb
+++ Formula/dotfiles-rs.rb
@@ -1,16 +1,15 @@
 class DotfilesRs < Formula
   desc "Dotfiles management framework"
   homepage "https://github.com/miguelandres/dotfiles"
-  url "https://api.github.com/repos/miguelandres/dotfiles-rs/tarball/nightly-2022-10-27"
+  url "https://github.com/miguelandres/dotfiles-rs/archive/refs/tags/nightly-2022-10-27.tar.gz"
   sha256 "7029e0f7fcdbc66ca754081aadcc740f4680ce04023413d58f5b702ea86a7f7a"
   license "MIT"
 
   depends_on "rustup-init" => :build
 
   def install
-    system "rustup-init", "-y", "--default-toolchain", "nightly", "--profile", "default" 
-    system "cargo", "build", "--release", "--bin", "dotfiles"
-    bin.install "target/release/dotfiles"
+    system "rustup-init", "-y", "--default-toolchain", "nightly", "--profile", "default"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
