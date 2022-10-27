77f1ebc62d1f9a4db24b8cdc62bbe32c9a8d64a9
Miguel Barreto <miguelandres@users.noreply.github.com>
2022-10-27 18:31:08 -0400

    remove version

--- Formula/dezoomify-rs.rb
+++ Formula/dezoomify-rs.rb
@@ -2,7 +2,6 @@ class DezoomifyRs < Formula
   desc "Zoomable image downloader for Google Arts & Culture, Zoomify, IIIF, and others"
   homepage "https://dezoomify-rs.ophir.dev"
   url "https://github.com/lovasoa/dezoomify-rs/archive/refs/tags/v2.9.2.tar.gz"
-  version "2.9.2"
   sha256 "33bdc75ada73faacd5551ecc7da2e4c99a6fa62fb7c9325db3d83b22e9eb8157"
   license "GPL-3.0-only"
 
