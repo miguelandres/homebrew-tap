0428dcd6288bd42f01f9e78e91756595973d17e1
Miguel Barreto <miguelandres@users.noreply.github.com>
2026-05-30 00:06:15 -0400

    fix shellcheck

--- download.sh
+++ download.sh
@@ -3,14 +3,15 @@
 os=${1="all_binaries"}
 repo=${2="miguelandres/dotfiles-rs"}
 
-URL=$(curl -s https://api.github.com/repos/$repo/releases/latest \
-| grep "browser_download_url.*$os" \
+# shellcheck disable=SC2312
+URL=$(curl -s "https://api.github.com/repos/${repo}/releases/latest" \
+| grep "browser_download_url.*${os}" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | cut -c 2-)
 
-echo "downloading $URL"
-curl "$URL" -L -o df.tar.gz
+echo "downloading ${URL}"
+curl "${URL}" -L -o df.tar.gz
 
 shasum -a 256 df.tar.gz
 rm df.tar.gz
